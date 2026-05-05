b372fe60e14811729da676679c4bbe8f5c75646c
Sferralove <sferra.angelo@gmail.com>
2026-05-05 08:36:24 +0200

    fix: minimal plugin, only tools, no hooks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { PluginStore } from './store/index.js';
-import { PluginContainer } from './plugin-container.js';
-import { createToolHooks } from './hooks/tool.js';
 import { createTools } from './tools/index.js';
 
 interface PluginInput {
@@ -9,10 +7,6 @@ interface PluginInput {
 
 export const server = async ({ directory }: PluginInput) => {
   const store = new PluginStore(directory);
-  const container = new PluginContainer();
-
-  const toolHooks = createToolHooks(store, container);
   const tools = createTools(store);
-
-  return { ...toolHooks, tool: tools };
+  return { tool: tools };
 };
